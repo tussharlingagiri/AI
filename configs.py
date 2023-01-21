147026ce38b5e7bdeb6663fd0eb4f5b80f4eb87d
tussharlion@gmail.com <tussharlion@gmail.com>
2023-01-21 07:41:34 +0000

    refactor

---
+++ configs.py
@@ -0,0 +1,36 @@
+config = {
+    "alpha_vantage": {
+        "key": "YOUR_API_KEY", # Claim your free API key here: https://www.alphavantage.co/support/#api-key
+        "symbol": "IBM",
+        "outputsize": "full",
+        "key_adjusted_close": "5. adjusted close",
+        # This line of code is specifying the key to use when
+        #  requesting adjusted closing price data from the Alpha Vantage API. The value "5. adjusted close" tells the API to use the key "5" when requesting data for the adjusted closing price. The adjusted closing price takes into account any corporate actions such as dividends, stock splits, etc. and is considered more accurate than the regular closing price when calculating performance and returns on investment.
+    },
+    "data": {
+        "window_size": 20,
+        "train_split_size": 0.80,
+    }, 
+    "plots": {
+        "xticks_interval": 90, # show a date every 90 days
+        "color_actual": "#001f3f",
+        "color_train": "#3D9970",
+        "color_val": "#0074D9",
+        "color_pred_train": "#3D9970",
+        "color_pred_val": "#0074D9",
+        "color_pred_test": "#FF4136",
+    },
+    "model": {
+        "input_size": 1, # since we are only using 1 feature, close price
+        "num_lstm_layers": 2,
+        "lstm_size": 32,
+        "dropout": 0.2,
+    },
+    "training": {
+        "device": "cpu", # "cuda" or "cpu"
+        "batch_size": 64,
+        "num_epoch": 100,
+        "learning_rate": 0.01,
+        "scheduler_step_size": 40,
+    }
+}
